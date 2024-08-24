988de15709b29157a86654dc1df5fd1b20916db2
Michael Collins <mikec1233@gmail.com>
2024-08-24 18:31:17 -0400

    load_documents functionality

---
+++ backend/src/load_documents.py
@@ -0,0 +1,14 @@
+from langchain.document_loaders import DirectoryLoader
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATA_PATH = os.environ['DATA_PATH']
+
+def load_documents():
+    loader = DirectoryLoader(DATA_PATH, glob="*md")
+    documents = loader.load()
+    return documents
+
+#print(load_documents())
+
