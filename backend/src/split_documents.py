4f1496ac798e7e952dcf995adbf8f008d5a21897
Michael Collins <mikec1233@gmail.com>
2024-08-25 00:04:31 -0400

    Functional Backend Logic Implemented

--- backend/src/split_documents.py
+++ backend/src/split_documents.py
@@ -1,14 +1,20 @@
 from load_documents import load_documents
+from langchain.schema import Document
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-'''text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=500,
-    length_function=len,
-    add_start_index=True,
-)'''
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
 
-'''
-This will be used later, currently our document is extremely small
-'''
+    document = chunks[10]
+    print(document.page_content)
+    print(document.metadata)
+
+    return chunks
