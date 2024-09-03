b4040d0efb6f1cbff7df6e88b7a63531c1fb6af9
Michael Collins <mikec1233@gmail.com>
2024-09-03 15:35:11 -0400

    Added list_query API and start frontend

--- backend/src/api/api_handler.py
+++ backend/src/api/api_handler.py
@@ -1,30 +1,43 @@
 import os
 import uvicorn
 import json
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from backend.src.database.db_model import DBQueryModel
 
 from backend.src.app_logic.query_data import query_rag
 
-
+CHARACTER_LIMIT = 2000
 app = FastAPI()
+
 
 class SubmitQueryRequest(BaseModel):
     query_text: str
+    user_id: Optional[str] = None
 
-@app.get("/")
-def index():
-    return{"Hello": "World"}
+# @app.get("/")
+# def index():
+#     return{"Hello": "World"}
 
 @app.get("/get_query")
 def get_query_endpoint(query_id: str) -> DBQueryModel:
     query = DBQueryModel.get_item(query_id)
-    return query
+    if query:
+        return query
+    else:
+        raise HTTPException(status_code=404, detail=f"Query Not Found: {query_id}")
 
 @app.post("/submit_query")
 def submit_query_endpoint(request: SubmitQueryRequest) -> DBQueryModel:
-    new_query = DBQueryModel(query_text=request.query_text)
+    if len(request.query_text) > CHARACTER_LIMIT:
+        raise HTTPException(status_code=400, detail=f"Query is too long. Character limit is: {CHARACTER_LIMIT}")
+    
+
+    #new_query = DBQueryModel(query_text=request.query_text)
+    user_id = request.user_id if request.user_id else "nobody"
+    new_query = DBQueryModel(query_text=request.query_text, user_id=user_id) 
+
     # Make a synchronous call to the worker (the RAG/AI app).
     query_response = query_rag(request.query_text)
     new_query.answer_text = query_response.response_text
@@ -34,6 +47,12 @@ def submit_query_endpoint(request: SubmitQueryRequest) -> DBQueryModel:
 
     return new_query
 
+@app.post("/list_queries")
+def list_query_endpoint(user_id: str) -> list[DBQueryModel]:
+    ITEM_COUNT = 5
+    print(f"Printing Quries for user: ${user_id}")
+    query_items = DBQueryModel.list_queries_by_user(user_id=user_id, count=ITEM_COUNT)
+    return query_items
 
 if __name__ == "__main__":
     # Run this as a server directly.
