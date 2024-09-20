8f61d7cb9341f25761790caeb07cf72d3506b064
Spencer Colon <0precision0@gmail.com>
2024-09-19 23:26:32 -0400

    Organizeed files into 'image' directory and added Dockerfile

---
+++ image/frontend/rag_frontend/src/api-client/models/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DBQueryModel';
+export * from './HTTPValidationError';
+export * from './SubmitQueryRequest';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
---
+++ spencer_rag_notes.md
@@ -0,0 +1,2 @@
+from RAGdemo : python -m backend.src.api.api_handler
+from /Users/spencercolon/GitRepos/RAGdemo/frontend/rag_frontend : npm run dev
