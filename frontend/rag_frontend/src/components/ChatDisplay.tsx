a282cbfb80f7fc96f1128342b85f6c1305474c52
Ryan Calderone <135069293+NotBurninGinger@users.noreply.github.com>
2024-10-07 14:47:37 -0400

    Added autoscroll to messages

--- frontend/rag_frontend/src/components/ChatDisplay.tsx
+++ frontend/rag_frontend/src/components/ChatDisplay.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef} from "react";
 
 interface ChatDisplayProps {
   responses: { user: string; bot: string }[];
   username: string;
 }
 
-const ChatDisplay: React.FC<ChatDisplayProps> = ({ responses, username }) => {
+const ChatDisplay: React.FC<ChatDisplayProps> = ({ responses, username }) =>  {
+  const chatEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (chatEndRef.current) {
+      chatEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [responses]);
+
   return (
-    <div className="chat-display">
+    <div className="chat-display"  style={{ overflowY: 'scroll', maxHeight: '400px', scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
       {responses.map((res, index) => (
         <div key={index} className="message">
           <div className="user-message-container">
-          <div className="user-message">
-            <strong>{username}:</strong> {res.user}
-          </div>
-        </div>
-          <div className="bot-message-container">
-          <div className="bot-message">
-            {res.bot}
+            <div className="user-message">
+              <strong>{username}:</strong> {res.user}
             </div>
+          </div>
+          <div className="bot-message-container">
+            <div className="bot-message">{res.bot}</div>
           </div>
         </div>
       ))}
+      <div ref={chatEndRef} />
     </div>
   );
 };
