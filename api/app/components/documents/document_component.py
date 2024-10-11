9b8eb7b8e95635d2ebe8904544646121a6d60e05
PatCerianDon <patrickcieran.donlon@gmail.com>
2024-10-10 21:05:13 -0400

    Lots of placeholder files, edit ingest and index type checking

--- api/app/components/documents/document_component.py
+++ api/app/components/documents/document_component.py
@@ -1,3 +1,4 @@
+from llama_index.core import SimpleDirectoryReader
 from llama_index.core import Document
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core.node_parser import SentenceSplitter
@@ -6,10 +7,11 @@ from llama_index.core.ingestion import IngestionPipeline, IngestionCache
 from llama_index.core.schema import BaseNode
 from typing import Sequence, List
 
-class IngestComponent:
-    def __init__(self, pipeline:IngestionPipeline):
-        self.pipeline = pipeline
+class DocumentComponent:
+    def __init__(self, input_files:List):
+        self.input_files = input_files
 
-    def ingest(self, documents:List[Document]) -> Sequence[BaseNode]:
-        return self.pipeline.run(documents=documents)
+    def load_documents(self) -> List[Document]:
+        return SimpleDirectoryReader(input_files=self.input_files)
     
+
