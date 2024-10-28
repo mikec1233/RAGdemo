13bc221b72d83f4bffafa678888db7c58cff03f3
Michael Collins <mikec1233@gmail.com>
2024-10-27 21:26:23 -0400

    Added chat_service functionality and llm_component

--- api/app/server/chat/chat_service.py
+++ api/app/server/chat/chat_service.py
@@ -1,5 +1,7 @@
+from dataclasses import asdict, dataclass
 from app.components.index.index_component import IndexingComponent
 from app.components.client.client_component import ClientComponent
+from app.components.llm.llm_component import LLMComponent
 from llama_index.vector_stores.opensearch import OpensearchVectorStore, OpensearchVectorClient
 from llama_index.core import VectorStoreIndex
 from llama_index.core.base import base_query_engine
@@ -12,9 +14,41 @@ class ChatService:
         self.vector_store=OpensearchVectorStore(self.client)
         self.index=IndexingComponent(vector_store=self.vector_store,settings=global_settings).index
         self.query_engine=self.index.as_query_engine()
+        self.llm_component = LLMComponent(global_settings)
+
+    #this method handles the logic for generating the responsve given the users question and the relavent docs from OpenSearch.        
+
+    def generate_response(self, context: str, question: str) -> str:
+        #we get our promp declared in the llm component.
+        prompt = self.llm_component.PROMPT_TEMPLATE.format(context=context, question=question)
+        #we generate and return the response from the llm
+        #We are using .complete right now for simple question answering. In the future we may want to switch to "chat" so that users
+        #can continue to ask questions based on prior context.
+        response = self.llm_component.llm.complete(prompt)
+        return response.text
+
 
     def query(self, user_input: str) -> dict:
+            
+            
+            #take user input, send to query_engine and get the result (later change to top 3 results)
             query_result = self.query_engine.query(user_input)
-            #return query_result
-            return {"response": query_result.response}
-    
+            print(f"Query result type: {type(query_result)}")  # Debugging
+            print(f"Query result content: {query_result}")  # Debugging
+
+
+            # Check for relevance threshold
+            if hasattr(query_result, "source_nodes") and query_result.source_nodes:
+                context_text = "\n\n---\n\n".join([node.node.text for node in query_result.source_nodes])
+            else:
+                return {"response": "No suitable results found"}
+
+
+            #using context_text and the initial question generate llm response
+            llm_response = self.generate_response(context=context_text, question=user_input)
+            print(f"LLM response type: {type(llm_response)}")  # Debugging
+
+
+
+            return {"response": llm_response}
+
