50318ee9fda7ba9920eb79d3f40283407a1c23f5
Michael Collins <mikec1233@gmail.com>
2024-11-06 21:11:03 -0500

    Bug fixes

--- backend/app/settings/settings.py
+++ backend/app/settings/settings.py
@@ -104,8 +104,8 @@ class Settings(BaseModel):
 
 global_settings = Settings(
     index=IndexSettings(
-        #endpoint="http://opensearch:9200",
-        endpoint="http://localhost:9200",
+        endpoint="http://opensearch:9200",
+        #endpoint="http://localhost:9200",
         index="docdemo",
         dim=1536,
         embedding_field="embedding",
