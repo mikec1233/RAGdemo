e27eaa56b6126714389575acd3d2440257a78d2e
mikec1233 <mcolls1233@gmail.com>
2024-11-07 19:36:22 -0500

    Visual cleanup

--- frontend/rag_frontend/src/components/ChatDisplay.tsx
+++ frontend/rag_frontend/src/components/ChatDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import TextWithCodeBlocks from "./TextWithCodeBlocks";
 
 interface ChatDisplayProps {
@@ -6,7 +6,7 @@ interface ChatDisplayProps {
   username: string;
 }
 
-const ChatDisplay: React.FC<ChatDisplayProps> = ({ responses, username }) =>  {
+const ChatDisplay: React.FC<ChatDisplayProps> = ({ responses, username }) => {
   const chatEndRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -16,16 +16,17 @@ const ChatDisplay: React.FC<ChatDisplayProps> = ({ responses, username }) =>  {
   }, [responses]);
 
   return (
-    <div className="chat-display"  style={{ overflowY: 'scroll', scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+    <div className="chat-display" style={{ overflowY: 'scroll', scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
       {responses.map((res, index) => (
         <div key={index} className="message">
           <div className="user-message-container">
             <div className="user-message">
-              <strong>{username}:</strong> {res.user}
+              <strong style={{ fontWeight: 'bold', color: '#000' }}>{username}:</strong> {/* User's name is bold */}
+              <span style={{ fontWeight: 'normal'}}> {res.user}</span> {/* User input text */}
             </div>
           </div>
           <div className="bot-message-container code-block-wrapper">
-              <TextWithCodeBlocks text={res.bot} />
+            <TextWithCodeBlocks text={res.bot} />
           </div>
         </div>
       ))}
